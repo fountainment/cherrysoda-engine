8f5f53b84cb760385de4479e35f1d9bad98750cd
C.Even <fountainment@gmail.com>
2019-10-05 18:46:13 +0800

    Update Log

--- Engine/CherrySoda/Utility/Log.cpp
+++ Engine/CherrySoda/Utility/Log.cpp
@@ -1,1 +1,23 @@
 #include <CherrySoda/Utility/Log.h>
+
+#include <CherrySoda/Utility/String.h>
+
+#ifdef _WIN32
+	#include <Windows.h>
+#else
+	#include <cstdio>
+#endif
+
+using cherrysoda::Log;
+
+using cherrysoda::String;
+
+void Log::DebugOutput(const String& output)
+{
+	std::printf(output.c_str());
+}
+
+void Log::LogOutput(const String& output)
+{
+	std::printf(output.c_str());
+}
--- Engine/CherrySoda/Utility/Log.h
+++ Engine/CherrySoda/Utility/Log.h
@@ -1,12 +1,15 @@
 #ifndef _CHERRYSODA_LOG_H_
 #define _CHERRYSODA_LOG_H_
 
+#include <CherrySoda/Utility/String.h>
+
 namespace cherrysoda {
 
 class Log
 {
 public:
-	//static void DebugOutput();
+	static void DebugOutput(const String& output);
+	static void LogOutput(const String& output);
 };
 
 } // namespace cherrysoda
