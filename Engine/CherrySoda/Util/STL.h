9bab0dd034a68cabee465f06f7b321e02ed551e2
Shen Yi Wen <c.even@live.cn>
2019-11-20 02:23:58 +0800

    Try fix clang compilation

--- Engine/CherrySoda/Util/STL.h
+++ Engine/CherrySoda/Util/STL.h
@@ -27,37 +27,37 @@ public:
 	template <typename T>
 	using Vector = std::vector<T>;
 
-	template<class T>
+	template<typename T>
 	static void Resize(T& container, size_t size)
 	{
 		container.resize(size);
 	}
 
-	template<class T, class U>
+	template<typename T, typename U>
 	static void Fill(T& container, const U& element)
 	{
 		std::fill(container.begin(), container.end(), element);
 	}
 
-	template<class T, class U>
+	template<typename T, typename U>
 	static void Add(T& container, const U& element)
 	{
 		container.emplace_back(element);
 	}
 
-	template<class T>
+	template<typename T>
 	static void Add(HashSet<T>& container, const T& element)
 	{
 		container.emplace(element);
 	}
 
-	template<class T, class U>
+	template<typename T, typename U>
 	static void AddRange(T& container, const U& iterable)
 	{
 		container.insert(std::end(container), std::begin(iterable), std::end(iterable));
 	}
 
-	template<class T, class U>
+	template<typename T, typename U>
 	static bool Remove(T& container, const U& element)
 	{
 		auto it = Find(container, element);
@@ -68,37 +68,37 @@ public:
 		return true;
 	}
 
-	template<class T>
+	template<typename T>
 	static size_t Count(const T& container)
 	{
 		return container.size();
 	}
 
-	template<class T>
+	template<typename T>
 	static auto Data(const T& container)
 	{
 		return container.data();
 	}
 
-	template<class T>
+	template<typename T>
 	static size_t ByteSize(const T& container)
 	{
 		return Count(container) * sizeof T::value_type;
 	}
 
-	template<class T, class U>
+	template<typename T, typename U>
 	static auto Find(const T& container, const U& element)
 	{
 		return std::find(std::begin(container), std::end(container), element);
 	}
 
-	template<class T>
+	template<typename T>
 	static auto Find(std::unordered_set<T>& container, const T& element)
 	{
 		return container.find(element);
 	}
 
-	template<class T, class U>
+	template<typename T, typename U>
 	static bool Contains(const T& container, const U& element)
 	{
 		return Find(container, element) != container.end();
