db2304df5231254184d69dc85cd851ac0c335bf2
Shen Yi Wen <c.even@live.cn>
2019-11-20 02:00:42 +0800

    Update Math.h

--- Playground/VoxelExperiment/Scenes/MainScene.cpp
+++ Playground/VoxelExperiment/Scenes/MainScene.cpp
@@ -4,12 +4,14 @@
 #include <CherrySoda/Components/Component.h>
 #include <CherrySoda/Renderers/Renderer.h>
 #include <CherrySoda/Renderers/EverythingRenderer.h>
+#include <CherrySoda/Util/Math.h>
 
 #include "Voxel/Chunk.h"
 
 using cherrysoda::Component;
 using cherrysoda::Entity;
 using cherrysoda::EverythingRenderer;
+using cherrysoda::Math;
 using cherrysoda::Renderer;
 
 void MainScene::Begin()
@@ -17,8 +19,7 @@ void MainScene::Begin()
 	auto renderer = new EverythingRenderer;
 	auto entity = new Chunk;
 
-	*(renderer->GetCamera()) = cherrysoda::Camera(1, 1);
-	renderer->GetCamera()->Position(cherrysoda::Math::Vec3(0.f, 0.f, 10.f));
+	renderer->GetCamera()->Position(Math::Vec3(0.f, 0.f, 3.f));
 
 	Add(renderer);
 	Add(entity);
