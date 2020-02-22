695e6d9fe0f6755e6386c42fbbd0c15c49c4c115
Shen Yi Wen <c.even@live.cn>
2020-02-22 19:13:27 +0800

    Add SDL2 zip

--- Tools/setup_sdl2_for_windows.py
+++ Tools/setup_sdl2_for_windows.py
@@ -1,14 +1,11 @@
 import lib.cherrysoda as cherry
 
 def main():
-    # Download
-    sdl2_download_link = 'https://www.libsdl.org/release/SDL2-devel-2.0.10-VC.zip'
-    sdl2_zip_filename = cherry.download_url_to(sdl2_download_link, cherry.join_path(cherry.tool_path, 'tmp'))
+    sdl2_zip_filename = cherry.join_path(cherry.tool_path, 'config\\SDL2-devel-2.0.10-VC.zip')
     cherry.extract_zip_to(sdl2_zip_filename, cherry.external_path)
-    # Setup
     sdl2_dir = cherry.join_path(cherry.external_path, 'SDL2-2.0.10')
     sdl2_header_dir = cherry.join_path(sdl2_dir, 'include\\SDL2\\')
-    cherry.copy(cherry.join_path(cherry.tool_path, 'cfg\\sdl2-config.cmake'), sdl2_dir)
+    cherry.copy(cherry.join_path(cherry.tool_path, 'config\\sdl2-config.cmake'), sdl2_dir)
     cherry.make_sure_folder_exist(sdl2_header_dir)
     cherry.move(cherry.join_path(sdl2_dir, 'include\\*.*'), sdl2_header_dir)
     cherry.set_environment_variable('SDL2_DIR', sdl2_dir)
