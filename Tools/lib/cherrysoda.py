7e60a3a5494a5eec596518f96d299cd868c09014
Shen Yi Wen <c.even@live.cn>
2020-03-26 18:24:38 +0800

    Update CMakeLists.txt

--- Tools/lib/cherrysoda.py
+++ Tools/lib/cherrysoda.py
@@ -7,22 +7,28 @@ import subprocess
 import sys
 import zipfile
 
+
 if sys.version_info.major == 3:
     import urllib.request as urllib2
 else:
     import urllib2
 
+
 def is_windows_system():
     return platform.system() == 'Windows'
+
 
 def join_path(a, b):
     return os.path.join(a, b)
 
+
 def abspath(a):
     return os.path.abspath(a)
 
+
 def get_file_path(f):
     return os.path.dirname(f)
+
 
 executable_suffix = ('', '.exe')[is_windows_system()]
 
@@ -33,13 +39,16 @@ bgfx_src_path = join_path(external_path, 'bgfx/bgfx/src')
 
 shaderc = join_path(tool_path, 'bin/shaderc' + executable_suffix)
 
+
 def make_sure_folder_exist(f):
     p = os.path.dirname(f)
     pathlib.Path(p).mkdir(parents=True, exist_ok=True)
 
+
 def execute_command(command):
     # print('$ ' + ' '.join(command))
     subprocess.run(command)
+
 
 def compile_shader(shader_source, output, platform, shader_type, include_path=None, profile=None, opt_level=None):
     command = [shaderc, '-f', shader_source, '-o', output, '--platform', platform, '--type', shader_type]
@@ -51,6 +60,7 @@ def compile_shader(shader_source, output, platform, shader_type, include_path=No
         command += ['-O', str(opt_level)]
     make_sure_folder_exist(output)
     execute_command(command)
+
 
 def download_url_to(url, dest):
     filename = url.split('/')[-1]
@@ -64,6 +74,7 @@ def download_url_to(url, dest):
     print("Finished!")
     return filedest
 
+
 def extract_zip_to(filename, dest):
     zfile = zipfile.ZipFile(filename, 'r')
     for ef in zfile.namelist():
@@ -76,19 +87,28 @@ def extract_zip_to(filename, dest):
         f.close()
     zfile.close()
 
+
 def get_file_list_from_wildcard(wildcard):
     return glob.glob(wildcard)
+
 
 def copy(src, dest):
     src_list = get_file_list_from_wildcard(src)
     for source in src_list:
         shutil.copy(source, dest)
 
+
 def move(src, dest):
     src_list = get_file_list_from_wildcard(src)
     for source in src_list:
         shutil.move(source, dest)
 
+
 def set_environment_variable(env_var, value):
     command = ['setx', env_var, value]
     execute_command(command)
+
+def write_str_file(str, dest):
+    f = open(dest, 'w')
+    f.write(str)
+    f.close()
--- Tools/setup_sdl2_for_windows.py
+++ Tools/setup_sdl2_for_windows.py
@@ -1,5 +1,6 @@
 import lib.cherrysoda as cherry
 
+
 def main():
     sdl2_zip_filename = cherry.join_path(cherry.tool_path, 'config\\SDL2-devel-2.0.10-VC.zip')
     cherry.extract_zip_to(sdl2_zip_filename, cherry.external_path)
@@ -9,6 +10,7 @@ def main():
     cherry.make_sure_folder_exist(sdl2_header_dir)
     cherry.move(cherry.join_path(sdl2_dir, 'include\\*.*'), sdl2_header_dir)
     cherry.set_environment_variable('SDL2_DIR', sdl2_dir)
+
 
 if __name__ == '__main__':
     main()
