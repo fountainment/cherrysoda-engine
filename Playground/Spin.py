88867ad240e79e277074dfd91cd6567e54e496c0
Shen Yi Wen <c.even@live.cn>
2020-12-07 22:40:20 +0800

    Update CMakeLists.txt

--- Playground/Spin.py
+++ Playground/Spin.py
@@ -1,33 +1,29 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def get_spin(s, a, b, c):
+def get_spin(radius_scale, height_scale, rounds):
     xs, ys, zs = [], [], []
-    theta = s
-    for i in range(50):
-        delta = 0.5
-        t = theta + a
-        if t > 0:
-            delta = np.clip(1.0 / t, 0.25, delta)
+    theta = 0.0
+    delta = 0.1
+    twopi = math.pi * 2.0
+    for i in range(int(rounds * twopi / delta)):
         theta += delta
-        t = theta
-        if t < 0:
-            t = 0
-        x = np.cos(t + a) * t * b
-        y = np.sin(t + a) * t * b
+        radius = theta / twopi * radius_scale
+        x = np.cos(theta) * radius
+        y = np.sin(theta) * radius
         xs.append(x)
         ys.append(y)
-        zs.append(t * c)
+        zs.append(theta / twopi * height_scale)
     return xs, ys, zs
 
 def main():
     fig = plt.figure()
     ax = Axes3D(fig)
 
-    ax.plot(*get_spin(0.0, 0.0, 1.0, 3.0))
-    ax.plot(*get_spin(-5.28, 5.28, 1.0, 3.0))
-    # ax.plot(*get_spin(-6.28, 0.0, 0.96, 3.0))
+    ax.plot(*get_spin(1.0,  3.0,  5.0))
+    ax.plot(*get_spin(1.05, 3.15, 5.0))
     plt.show()
 
 if __name__ == '__main__':
