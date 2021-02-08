6296e1ded52041d95c937b353b22f27d4a885472
Shen Yi Wen <c.even@live.cn>
2021-02-08 19:05:23 +0800

    Camera: Fix scale functionality

--- Engine/CherrySoda/Util/Camera.cpp
+++ Engine/CherrySoda/Util/Camera.cpp
@@ -25,13 +25,14 @@ Camera::Camera(int width, int height)
 void Camera::UpdateMatrices()
 {
 	if (m_changed) {
-		const Math::Vec3 actualPos = m_position - m_origin;
-		m_viewMatrix = Math_Scale(Math_LookAt(actualPos, actualPos + m_direction, Math_RotateVector(m_upVector, ZRotation(), Vec3_ZUp)), m_zoom);
 		if (UseOrthoProjection()) {
+			m_viewMatrix = Math_Translate(Math_Scale(Math_Rotate(Math_Translate(Math_Identity<Math::Mat4>(), Origin()), -ZRotation(), Vec3_ZUp), Scale()), -Position());
 			m_projMatrix = Math_Ortho(0.f, m_width, 0.f, m_height, -10000.f, 10000.f);
 		}
 		else {
-			m_projMatrix = Math_Perspective(Math_Radians(m_fov), Ratio(), 0.1f, 10000.f);
+			const Math::Vec3 actualPos = Position() - Origin();
+			m_viewMatrix = Math_LookAt(actualPos, actualPos + Direction(), Math_RotateVector(GetUpVector(), ZRotation(), Vec3_ZUp));
+			m_projMatrix = Math_Scale(Math_Perspective(Math_Radians(FOV()), Ratio(), 0.1f, 10000.f), m_zoom);
 		}
 		m_inverseMatrix = Math_Inverse(m_projMatrix * m_viewMatrix);
 		m_changed = false;
