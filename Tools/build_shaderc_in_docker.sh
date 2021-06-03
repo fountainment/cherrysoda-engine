cc3e6b8560f1faa8d8aa21164301a267a80f4faa
C.Even <c.even@live.cn>
2021-06-03 23:20:35 +0800

    Update build_shaderc_in_docker.sh

--- Tools/build_shaderc_in_docker.sh
+++ Tools/build_shaderc_in_docker.sh
@@ -6,7 +6,7 @@ tar zxvf cmake-3.12.2-Linux-x86_64.tar.gz
 mv cmake-3.12.2-Linux-x86_64 /opt/cmake-3.12.2
 ln -sf /opt/cmake-3.12.2/bin/* /usr/bin/
 apt update
-apt install libsd2-dev
+apt -y install libsd2-dev
 mkdir ~/cherrysoda-engine/build
 cd ~/cherrysoda-engine/build
 cmake .. -DCHERRYSODA_BUILD_SHADERC=1 -DCHERRYSODA_USE_SSE2=1
