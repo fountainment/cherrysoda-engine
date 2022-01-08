23147f75b50fc1a12607f478d63243cc07573ba9
C.Even <c.even@live.cn>
2022-01-08 19:00:38 +0800

    Integrate tinyxml2

---
+++ Engine/CherrySoda/Util/XML.cpp
@@ -0,0 +1,18 @@
+#include <CherrySoda/Util/XML.h>
+
+using cherrysoda::String;
+using cherrysoda::XMLUtil;
+
+
+bool XMLUtil::ReadXMLFile(cherrysoda::xml::XMLDocument& doc, const String& filename)
+{
+	cherrysoda::xml::XMLError result = doc.LoadFile(filename.c_str());
+	if (result != cherrysoda::xml::XMLError::XML_SUCCESS)
+	{
+		// Clean up.
+		doc.Clear();
+		return false;
+	}
+
+	return true;
+}
---
+++ Engine/CherrySoda/Util/XML.h
@@ -0,0 +1,25 @@
+#ifndef _CHERRYSODA_UTIL_XML_H_
+#define _CHERRYSODA_UTIL_XML_H_
+
+#include <CherrySoda/Util/String.h>
+
+#include <tinyxml2.h>
+
+namespace cherrysoda {
+
+namespace xml {
+
+using namespace ::tinyxml2;
+
+} // namespace json
+
+class XMLUtil
+{
+public:
+	static bool ReadXMLFile(xml::XMLDocument& doc, const String& filename);
+};
+
+} // namespace cherrysoda
+
+
+#endif // _CHERRYSODA_UTIL_XML_H_
