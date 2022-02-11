f5ab94667aeebe2a287c4d0b9c124bbf04525b36
C.Even <c.even@live.cn>
2022-02-12 00:12:42 +0800

    Add loading progress display to emscripten build

--- Tools/generate_index_html.py
+++ Tools/generate_index_html.py
@@ -12,8 +12,10 @@ def generate_simple_index_file(executable_name, index_file_path):
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <style>
             * { margin:0; padding:0; }
+            body { background-color: black; }
             html, body { width: 100%; height: 100%; }
-            #canvas { height: 100vh; width: 100vw; display: block; border: 0px; }
+            #loading_ui { position: absolute; z-index: -1; }
+            #loading_ui, #canvas { height: 100vh; width: 100vw; display: block; border: 0px; }
         </style>
         <script type='text/javascript'>
             window.onload = focuswindow;
@@ -22,8 +24,44 @@ def generate_simple_index_file(executable_name, index_file_path):
         </script>
     </head>
     <body>
-        <canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()"></canvas>
-        <script src="${EXECUTABLE_NAME}.js"></script>
+        <div class="emscripten_border">
+            <canvas id="loading_ui"></canvas>
+            <canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()" tabindex="-1"></canvas>
+        </div>
+        <script>
+            var loading_ui_canvas = document.getElementById("loading_ui");
+            var ctx = loading_ui_canvas.getContext("2d");
+
+            ctx.fillStyle = '#000000';
+            ctx.fillRect(0, 0, 300, 150);
+
+            ctx.fillStyle = "#ffffff";
+            ctx.fillRect(18, 71, 1, 8);
+            ctx.fillRect(281, 71, 1, 8);
+            ctx.fillRect(18, 71, 264, 1);
+            ctx.fillRect(18, 78, 264, 1);
+
+            var Module = {
+                preRun: [],
+                postRun: [],
+                setStatus: function(text) {
+                    if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
+                    if (text === Module.setStatus.last.text) return;
+                    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+                    var now = Date.now();
+                    if (m && now - Module.setStatus.last.time < 30) return;
+                    Module.setStatus.last.time = now;
+                    Module.setStatus.last.text = text;
+                    if (m) {
+                        var progress = parseInt(m[2]) / parseInt(m[4]);
+                        ctx.fillRect(20, 73, 260 * progress, 4);
+                    } else {
+                        loading_ui_canvas.style.display = 'none';
+                    }
+                }
+            };
+        </script>
+        <script async="" type="text/javascript" src="${EXECUTABLE_NAME}.js"></script>
     </body>
 </html>
 '''
