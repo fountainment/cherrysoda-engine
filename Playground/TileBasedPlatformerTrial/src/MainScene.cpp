56452b5fc05e74290e91a3ac7c8f0ca10a9b8faa
C.Even <c.even@live.cn>
2022-02-21 21:56:27 +0800

    Update TileBasedPlatformerTrial (WIP)
    * Add camera follow
    * Add variable tweaking slider

--- Playground/TileBasedPlatformerTrial/src/MainScene.cpp
+++ Playground/TileBasedPlatformerTrial/src/MainScene.cpp
@@ -10,6 +10,11 @@ using main::MainScene;
 
 static BitTag s_solidTag("solid");
 
+CHERRYSODA_CONSOLE_VARIABLE(actor_drop_acceleration, float, 1500.f, "");
+CHERRYSODA_CONSOLE_VARIABLE(actor_extra_drop_acceleration, float, 400.f, "");
+CHERRYSODA_CONSOLE_VARIABLE(actor_jump_speed, float, 360.f, "");
+CHERRYSODA_CONSOLE_VARIABLE(actor_horizontal_move_speed, float, 100.f, "");
+
 class Actor : public Entity
 {
 public:
@@ -81,6 +86,25 @@ private:
 	bool m_isAnythingAbove = false;
 };
 
+class CameraFollow : public Component
+{
+public:
+	CHERRYSODA_DECLARE_COMPONENT(CameraFollow, Component);
+
+	CameraFollow(Camera* camera)
+		: base(true, false)
+		, m_camera(camera)
+	{}
+
+	void Update() override
+	{
+		m_camera->Approach(Math_Round(GetEntity()->Position2D() - m_camera->GetSize() / m_camera->Scale2D() * 0.5f), 0.3f);
+	}
+
+private:
+	Camera* m_camera = nullptr;
+};
+
 class PlayerControl : public Component
 {
 public:
@@ -94,10 +118,10 @@ public:
 		float deltaTime = Engine::Instance()->DeltaTime();
 		bool jumpButtonPressed = MInput::GamePads(0)->Pressed(Buttons::A) || MInput::Keyboard()->Pressed(Keys::Space);
 		bool jumpButtonCheck = MInput::GamePads(0)->Check(Buttons::A) || MInput::Keyboard()->Check(Keys::Space);
-		float extraDropSpeed = -Math_Min(MInput::GamePads(0)->GetLeftStick().y, 0.f) * 300.f;
-		if (MInput::Keyboard()->Check(Keys::S)) extraDropSpeed = 300.f;
+		float extraDropSpeed = -Math_Min(MInput::GamePads(0)->GetLeftStick().y, 0.f) * actor_extra_drop_acceleration;
+		if (MInput::Keyboard()->Check(Keys::S)) extraDropSpeed = actor_extra_drop_acceleration;
 		if (jumpButtonCheck) {
-			extraDropSpeed -= 400.f;
+			extraDropSpeed -= actor_extra_drop_acceleration;
 		}
 		if (actor->IsOnGround()) {
 			m_speedY = 0.f;
@@ -106,23 +130,23 @@ public:
 			}
 		}
 		else {
-			m_speedY -= (1000.f + extraDropSpeed) * deltaTime;
+			m_speedY -= (actor_drop_acceleration + extraDropSpeed) * deltaTime;
 		}
 		if (actor->IsAnythingAbove()) {
 			if (m_speedY > 0.f) {
 				m_speedY = 0.f;
 			}
 		}
-		float speedX = MInput::GamePads(0)->GetLeftStick(0.2f).x * 100.f;
+		float speedX = MInput::GamePads(0)->GetLeftStick(0.2f).x * actor_horizontal_move_speed;
 		if (!MInput::GamePads(0)->Attached() || speedX == 0.f) {
-			speedX = MInput::Keyboard()->AxisCheck(Keys::A, Keys::D) * 100.f;
+			speedX = MInput::Keyboard()->AxisCheck(Keys::A, Keys::D) * actor_horizontal_move_speed;
 		}
-		static_cast<Actor*>(GetEntity())->Move(Math::Vec2(speedX, m_speedY) * deltaTime);
+		actor->Move(Math::Vec2(speedX, m_speedY) * deltaTime);
 	}
 
 	void Jump()
 	{
-		m_speedY = 260.f;
+		m_speedY = actor_jump_speed;
 	}
 
 private:
@@ -155,6 +179,11 @@ private:
 
 void MainScene::Begin()
 {
+	Commands::ExecuteCommand("addslider actor_drop_acceleration 0 3000");
+	Commands::ExecuteCommand("addslider actor_extra_drop_acceleration 0 3000");
+	Commands::ExecuteCommand("addslider actor_jump_speed 0 1000");
+	Commands::ExecuteCommand("addslider actor_horizontal_move_speed 0 1000");
+
 	auto renderer = new EverythingRenderer();
 
 	auto camera = renderer->GetCamera();
@@ -209,6 +238,7 @@ void MainScene::Begin()
 	player->Add(new HollowRectGraphicsComponent(10, 16));
 	player->SetCollider(new Hitbox(10, 16));
 	player->PositionY(200.f);
+	player->Add(new CameraFollow(camera));
 	Add(player);
 
 	base::Begin();
